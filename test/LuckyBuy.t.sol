c74d430218a91984665148b1d3c51237b69bd36d
tenthirtyone <alex.sherbuck@magiceden.io>
2025-03-21 01:04:59 -0500

    unit tests

--- test/LuckyBuy.t.sol
+++ test/LuckyBuy.t.sol
@@ -4,8 +4,16 @@ pragma solidity 0.8.28;
 import "forge-std/Test.sol";
 import "src/LuckyBuy.sol";
 
+contract MockLuckyBuy is LuckyBuy {
+    constructor(uint256 protocolFee_) LuckyBuy(protocolFee_) {}
+
+    function setIsFulfilled(uint256 commitId_, bool isFulfilled_) public {
+        isFulfilled[commitId_] = isFulfilled_;
+    }
+}
+
 contract TestLuckyBuyCommit is Test {
-    LuckyBuy luckyBuy;
+    MockLuckyBuy luckyBuy;
     address admin = address(0x1);
     address user = address(0x2);
     address receiver = address(0x3);
@@ -42,7 +50,7 @@ contract TestLuckyBuyCommit is Test {
 
     function setUp() public {
         vm.startPrank(admin);
-        luckyBuy = new LuckyBuy(protocolFee);
+        luckyBuy = new MockLuckyBuy(protocolFee);
         vm.deal(admin, 100 ether);
         vm.deal(receiver, 100 ether);
         vm.deal(address(this), 100 ether);
@@ -958,6 +966,86 @@ contract TestLuckyBuyCommit is Test {
         vm.warp(block.timestamp + 2 days);
 
         luckyBuy.expireCommit(0);
+    }
+
+    function testExpireCommitNotOwner() public {
+        vm.startPrank(admin);
+        luckyBuy.setCommitExpireTime(1 days);
+        vm.stopPrank();
+
+        vm.deal(address(this), amount);
+        luckyBuy.commit{value: amount}(
+            address(this),
+            cosigner,
+            seed,
+            orderHash,
+            reward
+        );
+
+        vm.warp(block.timestamp + 2 days);
+
+        vm.expectRevert(LuckyBuy.InvalidCommitOwner.selector);
+        vm.prank(user);
+        luckyBuy.expireCommit(0);
+    }
+
+    function testExpireCommitAlreadyFulfilled() public {
+        vm.startPrank(admin);
+        luckyBuy.setCommitExpireTime(1 days);
+        vm.stopPrank();
+
+        vm.deal(address(this), amount);
+        luckyBuy.commit{value: amount}(
+            address(this),
+            cosigner,
+            seed,
+            orderHash,
+            reward
+        );
+
+        luckyBuy.setIsFulfilled(0, true);
+        assertEq(luckyBuy.isFulfilled(0), true);
+
+        vm.warp(block.timestamp + 2 days);
+
+        vm.expectRevert(LuckyBuy.AlreadyFulfilled.selector);
+        luckyBuy.expireCommit(0);
+    }
+
+    function testFulfillIsExpired() public {
+        vm.startPrank(admin);
+        luckyBuy.setCommitExpireTime(1 days);
+        vm.stopPrank();
+
+        vm.deal(address(this), 100 ether);
+        (bool success, ) = address(luckyBuy).call{value: 10 ether}("");
+        assertTrue(success, "Initial funding should succeed");
+        luckyBuy.commit{value: amount}(
+            address(this),
+            cosigner,
+            seed,
+            orderHash,
+            reward
+        );
+
+        assertEq(luckyBuy.isFulfilled(0), false);
+
+        vm.warp(block.timestamp + 2 days);
+
+        luckyBuy.expireCommit(0);
+
+        assertEq(luckyBuy.isExpired(0), true);
+
+        vm.expectRevert(LuckyBuy.CommitIsExpired.selector);
+        luckyBuy.fulfill(
+            0,
+            address(this),
+            new bytes(0),
+            reward,
+            address(0),
+            0,
+            new bytes(0)
+        );
     }
 
     receive() external payable {}
