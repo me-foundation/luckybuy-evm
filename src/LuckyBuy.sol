f7e27c94e20c1397aff59af6d47c9610a9cb45fd
tenthirtyone <alex.sherbuck@magiceden.io>
2025-05-15 13:47:53 -0500

    code organization

--- src/LuckyBuy.sol
+++ src/LuckyBuy.sol
@@ -261,6 +261,35 @@ contract LuckyBuy is
     /// @param token_ Address of the token being transferred (zero address for ETH)
     /// @param tokenId_ ID of the token if it's an NFT
     /// @param signature_ Signature used for random number generation
+    /// @dev Emits a Fulfillment event on success
+    function fulfill(
+        uint256 commitId_,
+        address marketplace_,
+        bytes calldata orderData_,
+        uint256 orderAmount_,
+        address token_,
+        uint256 tokenId_,
+        bytes calldata signature_
+    ) public payable nonReentrant whenNotPaused {
+        _fulfill(
+            commitId_,
+            marketplace_,
+            orderData_,
+            orderAmount_,
+            token_,
+            tokenId_,
+            signature_
+        );
+    }
+
+    /// @notice Fulfills a commit with the result of the random number generation
+    /// @param commitId_ ID of the commit to fulfill
+    /// @param marketplace_ Address where the order should be executed
+    /// @param orderData_ Calldata for the order execution
+    /// @param orderAmount_ Amount of ETH to send with the order
+    /// @param token_ Address of the token being transferred (zero address for ETH)
+    /// @param tokenId_ ID of the token if it's an NFT
+    /// @param signature_ Signature used for random number generation
     /// @param feeSplitReceiver_ Address of the fee split receiver
     /// @param feeSplitPercentage_ Percentage of the fee to split relative to the protocol fees paid
     /// @dev Emits a FeeSplit event on success
@@ -305,35 +334,6 @@ contract LuckyBuy is
             feeSplitPercentage_,
             protocolFeesPaid,
             splitAmount
-        );
-    }
-
-    /// @notice Fulfills a commit with the result of the random number generation
-    /// @param commitId_ ID of the commit to fulfill
-    /// @param marketplace_ Address where the order should be executed
-    /// @param orderData_ Calldata for the order execution
-    /// @param orderAmount_ Amount of ETH to send with the order
-    /// @param token_ Address of the token being transferred (zero address for ETH)
-    /// @param tokenId_ ID of the token if it's an NFT
-    /// @param signature_ Signature used for random number generation
-    /// @dev Emits a Fulfillment event on success
-    function fulfill(
-        uint256 commitId_,
-        address marketplace_,
-        bytes calldata orderData_,
-        uint256 orderAmount_,
-        address token_,
-        uint256 tokenId_,
-        bytes calldata signature_
-    ) public payable nonReentrant whenNotPaused {
-        _fulfill(
-            commitId_,
-            marketplace_,
-            orderData_,
-            orderAmount_,
-            token_,
-            tokenId_,
-            signature_
         );
     }
 
